bb21a467d43724339c0092a420b3b3601c196760
shoumanx <mshoman01020@gmail.com>
2024-03-26 21:46:04 +0200

    Edit\Update Room

--- controllers/room.js
+++ controllers/room.js
@@ -17,6 +17,27 @@ const addRoom = (req,res)=>{
   res.send(room);
 };
 
+
+const editRoom = (req,res)=>{
+  const room = rooms.find(r => r.id===parseInt(req.params.id));
+  if(!room){
+    res.status(404).send('The room with the given Id not found');
+  }
+  
+  const result = validateRoom(req.body);
+  
+  if(result.error){
+    res.status(400).send(result.error.details[0].message);
+    console.log(result);
+    return;
+  }
+  room.type = req.body.name;
+  res.send(room);
+};
+
+
+
 module.exports = {
-    addRoom
+    addRoom,
+    editRoom
 }
