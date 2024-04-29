efcf775442f782cd79e32c03e554250aeffa5708
shoumanx <mshoman01020@gmail.com>
2024-04-29 21:25:32 +0300

    Add Functions to User

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+<<<<<<< Updated upstream
 const app = express();
 app.use(express.json());
+=======
+const { roomRouter } = require("./routes/room_route");
+const { reservationRouter } = require("./routes/user_route");
+
+const app = express();
+app.use(express.json());
+app.use("/api/rooms/", roomRouter);
+app.use("/api/user/", reservationRouter);
+
+
+>>>>>>> Stashed changes
 
 const {roomRouter}=require('./routes/room_route')
 app.use('/api/rooms/',roomRouter);
---
+++ controllers/user_controller.js
@@ -0,0 +1,46 @@
+const { Reservation } = require('../models/reservation'); 
+
+
+//* mamdouh
+async function make_reservation(req, res) {
+    
+}
+
+//* mamdouh
+async function edit_reservation(req, res) {
+    
+}
+
+//* hesham
+async function cancel_reservation(req, res) {
+    
+}
+
+const getAllReservations = async (req, res) => {
+    try {
+        const allReservations = await Reservation.find({});
+        res.status(200).send(allReservations);
+    }catch(error){
+        res.status(400).send(error);
+    }
+};
+
+
+
+const getReservationById = async(req, res) => {
+    try {
+        const oneReservation = await Reservation.findOne({_id: req.params.id});
+        res.status(200).send(oneReservation);
+    }catch(error){
+        res.status(400).send(error);
+    }
+};
+
+
+module.exports = {
+    make_reservation,
+    edit_reservation,
+    cancel_reservation,
+    getAllReservations,
+    getReservationById,
+}
