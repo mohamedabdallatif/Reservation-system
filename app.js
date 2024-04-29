febd9ed3395fc3e25adc23356e0fad3ce756d93b
shoumanx <mshoman01020@gmail.com>
2024-04-29 21:27:45 +0300

    Add Functions to User

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const express = require("express");
-<<<<<<< Updated upstream
-const app = express();
-app.use(express.json());
-=======
 const { roomRouter } = require("./routes/room_route");
 const { reservationRouter } = require("./routes/user_route");
 
@@ -12,10 +8,6 @@ app.use("/api/rooms/", roomRouter);
 app.use("/api/user/", reservationRouter);
 
 
->>>>>>> Stashed changes
-
-const {roomRouter}=require('./routes/room_route')
-app.use('/api/rooms/',roomRouter);
 module.exports={
     app
 }
